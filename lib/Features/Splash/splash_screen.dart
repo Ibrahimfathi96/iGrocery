57deaf88787d2599b603354de457f474a8fc818b
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-11-29 17:11:57 +0200

    Retouches

--- lib/Features/Splash/splash_screen.dart
+++ lib/Features/Splash/splash_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:i_grocery/Core/Routes/routes.dart';
 import 'package:i_grocery/Core/Utils/app_assets.dart';
 import 'package:i_grocery/Core/Utils/app_colors.dart';
@@ -32,42 +31,12 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.sizeOf(context).width;
     return Scaffold(
         backgroundColor: AppColors.primaryColor,
         body: Center(
           child: Image.asset(
             AppAssets.logoIconImg,
           ),
-        )
-        //     Row(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: [
-        //     SvgPicture.asset(AppAssets.splashLogo),
-        //     SizedBox(width: width / 40),
-        //     const Column(
-        //       mainAxisAlignment: MainAxisAlignment.center,
-        //       children: [
-        //         Text(
-        //           "iGrocery",
-        //           style: TextStyle(
-        //             color: Colors.white,
-        //             fontWeight: FontWeight.bold,
-        //             fontSize: 36,
-        //           ),
-        //         ),
-        //         Text(
-        //           "online groceries",
-        //           style: TextStyle(
-        //             letterSpacing: 2,
-        //             color: Colors.white,
-        //             fontSize: 16,
-        //           ),
-        //         ),
-        //       ],
-        //     ),
-        //   ],
-        // ),
-        );
+        ));
   }
 }
